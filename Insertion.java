6cc653147f478e3a0a9df501ff867cee4f80233b
nurulhikmahida24 <nurulhikmahidayat@gmail.com>
2020-01-14 11:58:34 +0700

    fisrt commit

---
+++ Insertion.java
@@ -0,0 +1,25 @@
+public class Insertion{
+	public static void insertionSort(int[] array) {
+		for (int i=1; i < array.length; i++){
+			int temp = array[i];
+			int k;
+			
+			for (k=i-1; k >= 0 && array[k] > temp; k--) {
+				array[k+1] = array[k];
+			}
+			
+			array[k+1] = temp;
+			
+		}
+	}
+	
+	public static void main (String [] args) {
+		int[] array = {0, -2, 3, 1, 4, -4, 8, -6};
+		
+		Insertion.insertionSort(array);
+		for (int i=0; i< array.length; i++)
+		{
+			System.out.println("Elemen di Index" +i+ "Sekarang adalah" +array[i]);
+		}
+	}
+}
